761f5ffd99816c9a9a8ae4f1457279d4ac122bbe
Toma Alexandru <alextoma73.at@gmail.com>
2023-11-11 01:14:57 +0200

    First Setup

---
+++ app/Filament/Resources/ContactResource.php
@@ -0,0 +1,117 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\ContactResource\Pages;
+use App\Filament\Resources\ContactResource\RelationManagers;
+use App\Models\Contact;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Forms\Components\TextInput;
+use Filament\Forms\Components\Select;
+use Filament\Tables\Filters\SelectFilter;
+use Illuminate\Database\Eloquent\Builder;
+use Filament\Tables\Filters\Filter;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class ContactResource extends Resource
+{
+    protected static ?string $model = Contact::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-user-circle';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                TextInput::make('name')
+                    ->required()
+                    ->minLength(3)
+                    ->maxLength(20),
+                TextInput::make('phone_number')
+                    ->required()
+                    ->prefix('+')
+                    ->tel()->telRegex("/^[+]*[(]{0,1}[0-9]{1,4}[)]{0,1}[-\s\.\/0-9]*$/")
+                    ->length(10),
+                TextInput::make('email')
+                    ->required()
+                    ->email(),
+                Select::make('gender')
+                    ->options([
+                        'male' => 'Male',
+                        'female' => 'Female'
+                    ]),
+                TextInput::make('city')
+                    ->minLength(3),
+                Select::make('category')
+                    ->options([
+                        'family' => 'Family',
+                        'work' => 'Work',
+                        'school' => 'School',
+                        'friends' => 'Friends'
+                    ])
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                TextColumn::make('name')
+                    ->searchable()
+                    ->sortable(),
+                TextColumn::make('phone_number'),
+                TextColumn::make('email')->searchable(),
+                TextColumn::make('gender'),
+                TextColumn::make('city')->searchable(),
+                TextColumn::make('category')->searchable()
+            ])
+            ->filters([
+                // Filter::make('Male')
+                //     ->query(fn (Builder $query): Builder => $query->where('gender', 'male')),
+                // Filter::make('Female')
+                //     ->query(fn (Builder $query): Builder => $query->where('gender', 'female'))
+                SelectFilter::make('gender')
+                    ->options([
+                        'male' => 'Male',
+                        'female' => 'Female'
+                    ]),
+                SelectFilter::make('category')
+                    ->options([
+                        'work' => 'Work',
+                        'school' => 'School',
+                        'family' => 'Family',
+                        'friends' => 'Friends'
+                    ])
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+                Tables\Actions\DeleteAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListContacts::route('/'),
+            'create' => Pages\CreateContact::route('/create'),
+            'edit' => Pages\EditContact::route('/{record}/edit'),
+        ];
+    }
+}
---
+++ database/seeders/ContactSeeder.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\Contact;
+use Faker\Factory as Faker;
+
+class ContactSeeder extends Seeder
+{
+    public function run()
+    {
+        $faker = Faker::create();
+
+        // Define categories for contacts
+        $categories = ['work', 'school', 'family', 'friends'];
+
+        // Generate 50 contacts with random data
+        for ($i = 0; $i < 50; $i++) {
+            $contact = new Contact([
+                'name' => $faker->name,
+                'phone_number' => $this->generateRandomPhoneNumber(),
+                'gender' => $this->generateRandomGender(),
+                'city' => $faker->city,
+                'email' => $this->generateUniqueRandomEmail(),
+                'category' => $categories[array_rand($categories)],
+            ]);
+
+            $contact->save();
+        }
+    }
+
+    private function generateRandomPhoneNumber()
+    {
+        // Generate a random 10-digit phone number
+        return '555' . mt_rand(1000000, 9999999);
+    }
+
+    private function generateRandomGender()
+    {
+        // Generate a random gender (male or female)
+        $genders = ['male', 'female'];
+        return $genders[array_rand($genders)];
+    }
+
+    private function generateUniqueRandomEmail()
+    {
+        // Generate a unique email address using Laravel factory
+        return \Illuminate\Support\Str::random(10) . '@example.com';
+    }
+}
