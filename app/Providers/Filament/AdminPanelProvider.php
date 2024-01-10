dca21e91526b10cf72fa0b4658998b895a1a7f0f
Toma Alexandru <alextoma73.at@gmail.com>
2024-01-10 15:57:09 +0200

    Adding registration page + chage the default page to be /admin

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -27,6 +27,7 @@ class AdminPanelProvider extends PanelProvider
             ->id('admin')
             ->path('admin')
             ->login()
+            ->registration()
             ->colors([
                 'primary' => Color::Amber,
             ])
@@ -38,8 +39,8 @@ class AdminPanelProvider extends PanelProvider
             ->discoverWidgets(in: app_path('Filament/Widgets'), for: 'App\\Filament\\Widgets')
             ->widgets([
                 Widgets\AccountWidget::class,
-                Widgets\ChartWidget::class,
-                // Widgets\FilamentInfoWidget::class,
+                // Widgets\ChartWidget::class,
+                Widgets\FilamentInfoWidget::class,
             ])
             ->middleware([
                 EncryptCookies::class,
