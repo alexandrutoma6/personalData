37da0c929c80fe29c9051e0498a59d5cc8f6dca2
Toma Alexandru <alextoma73.at@gmail.com>
2024-05-14 13:17:42 +0300

    updtae code editor page with code challenges

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,8 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\FlashCard;
+use Faker\Factory as Faker;
 
 class DatabaseSeeder extends Seeder
 {
@@ -18,5 +20,28 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+        
+        \App\Models\CodeChallenge::create([
+            'title' => 'Check if 786598 is a prime number, print a boolean',
+            'answer' => 'false',
+            'status' => 'active'
+        ]);
+
+        \App\Models\CodeChallenge::create([
+            'title' => 'Print the first 10 prime numbers, separated by a comma',    
+            'answer' => '2, 3, 5, 7, 11, 13, 17, 19, 23, 29',
+            'status' => 'active'
+        ]);
+        \App\Models\CodeChallenge::create([
+            'title' => 'Print the smallest multiple of 31 and 55',    
+            'answer' => '1695',
+            'status' => 'active'
+        ]);
+
+        \App\Models\CodeChallenge::create([
+            'title' => 'Check if 699 is divisibile by 3, print a boolean',    
+            'answer' => 'true',
+            'status' => 'active'
+        ]);
     }
 }
--- resources/views/livewire/code-editor.blade.php
+++ resources/views/livewire/code-editor.blade.php
@@ -11,6 +11,7 @@ use Wiebenieuwenhuis\FilamentCodeEditor\Components\CodeEditor;
 use Filament\Forms\Components\Select;
 use Filament\Forms\Components\Group;
 use Livewire\Volt\Component;
+use App\Models\CodeChallenge;
 
 //https://www.jdoodle.com/subscribe-api the API compiler
 new class extends Component implements HasForms, HasActions {
@@ -19,10 +20,25 @@ new class extends Component implements HasForms, HasActions {
 
     public ?array $data = [];
     public $output;
+    public $codeChallenge;
 
     public function mount(): void
     {
         $this->form->fill();
+        
+        // Check if there is any active challenge
+        $activeChallenge = CodeChallenge::where('status', 'active')->first();
+
+        if (!$activeChallenge) {
+            // If no active challenge found, make all challenges active
+            CodeChallenge::where('status', '!=', 'active')->update(['status' => 'active']);
+
+            // Set the first challenge as the active challenge
+            $this->codeChallenge = CodeChallenge::first();
+        } else {
+            // If an active challenge is found, set it as the active challenge
+            $this->codeChallenge = $activeChallenge;
+        }
     }
 
     public function form(Form $form): Form
@@ -75,6 +91,29 @@ new class extends Component implements HasForms, HasActions {
 }; ?>
 
 <div class="px-2">
+    {{-- CHALLENGE STYLE --}}
+    <style>
+        .container {
+            margin: 10px;
+        }
+        .title {
+            font-weight: bold;
+            color: #333; /* or any other color */
+        }
+        .content {
+            font-style: italic;
+            color: #666; /* or any other color */
+        }
+    </style>
+
+    {{-- CHALLENGE CONTENT --}}
+    <div class="container">
+        <div>
+            <p class="title">Daily Code Challenge:</p>
+            <p class="content">{{ $codeChallenge->title }}</p>
+        </div>
+    </div>
+
     <div class="py-4 ">
         {{ $this->form }}
     </div>
@@ -83,20 +122,34 @@ new class extends Component implements HasForms, HasActions {
         {{ $this->compileAction() }}
     </div>
     @if (isset($this->output))
-        @if (str_contains($this->output, 'NameError'))
-             @php
-                $position = strpos($this->output, 'NameError');
-                $substring = substr($this->output, $position + strlen('NameError:'));
-                $substring = ucfirst($substring);
+        @if ($this->output === $codeChallenge->answer)
+        {{-- set the challenge status to completed --}}
+            @php
+                $codeChallenge->status = 'completed';
+                $codeChallenge->save();
             @endphp
-
-            <div class="italic font-bold border border-gray-200 rounded-lg p-2 bg-gray-100">
-                {{ $substring }}
+            <div style="color: green" class="font-bold border border-gray-200 rounded-lg p-2 bg-gray-100">
+                <p class="p-2 font-bold">{{ $this->output }}</p>
             </div>
         @else
-            <div class="font-bold border border-gray-200 rounded-lg p-2 bg-gray-100">
-                <p class="p-2 font-bold"> {{ $this->output }} </p>
-            </div>
+        {{-- display the error message --}}
+            @if (str_contains($this->output, 'NameError'))
+                @php
+                    $position = strpos($this->output, 'NameError');
+                    $substring = substr($this->output, $position + strlen('NameError:'));
+                    $substring = ucfirst($substring);
+                @endphp
+
+                <div style="color: red" class="italic font-bold border border-gray-200 rounded-lg p-2 bg-gray-100">
+                    {{ $substring }}
+                </div>
+            @else
+            {{-- display the result with error --}}
+                <div style="color: red" class="font-bold border border-gray-200 rounded-lg p-2 bg-gray-100">
+                    <p class="p-2 font-bold">{{ $this->output }} // The answer is wrong, try again</p>
+                </div>
+            @endif
         @endif
     @endif
+
 </div>
